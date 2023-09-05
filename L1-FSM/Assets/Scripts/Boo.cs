93738bbc9595c0ed6a61f43c8d35a530d4ae9263
anton <antje12@student.sdu.dk>
2023-09-05 17:12:34 +0200

    l1

---
+++ L1-FSM/Assets/Scripts/Boo.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boo : Agent
+{
+    private GameObject mario;
+    private Transform tMario;
+    public float minimumDistance = 6f;
+    public float minimumAngle = 80f;
+    public float range = 3;
+
+    private void Start()
+    {
+        mario = GameObject.Find("Mario");
+        tMario = mario.transform;
+    }
+
+    protected override void FiniteStateMachine()
+    {
+        switch (state)
+        {
+            case 0: //Idle
+                if (Vector3.Distance(transform.position, tMario.position) < range)
+                    state = 1;
+                break;
+            case 1:
+                if (!CanMarioSeeMe())
+                    MoveTo(tMario);
+                if (Vector3.Distance(transform.position, tMario.position) > range)
+                    state = 0;
+                break;
+            default:
+                break;
+        }
+    }
+
+    bool CanMarioSeeMe()
+    {
+        //Debug.Log(Vector3.Distance(transform.position, mario.position));
+        //Debug.Log(Vector3.Angle(transform.position, mario.forward));
+        if (Vector3.Distance(transform.position, tMario.position) < minimumDistance &&
+            Vector3.Angle(transform.position, tMario.forward) < minimumAngle)
+        {
+            Debug.Log("He sees me!");
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ L1-FSM/Assets/Scripts/Mario.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario : Agent
+{
+    public int score = 0;
+    public float range = 2;
+
+    protected override void FiniteStateMachine()
+    {
+        GameObject boo = IsBooClose();
+        state = boo != null ? 1 : 0;
+        MoveTo(GetClosestCoin());
+        switch (state)
+        {
+            case 0:
+                MoveTo(GetClosestCoin());
+                break;
+            case 1:
+                MoveTo(FleeFrom(boo));
+                break;
+        }
+    }
+
+    GameObject IsBooClose()
+    {
+        GameObject[] boos = GameObject.FindGameObjectsWithTag("Boo");
+        float minDistance = range;
+        GameObject closestBoo = null;
+        foreach (GameObject boo in boos)
+        {
+            float distance = Vector3.Distance(boo.transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestBoo = boo;
+            }
+        }
+        return closestBoo;
+    }
+
+    Transform GetClosestCoin()
+    {
+        GameObject[] coins = GameObject.FindGameObjectsWithTag("Coin");
+        float minDistance = float.PositiveInfinity;
+        GameObject closestCoin = null;
+        foreach (GameObject coin in coins)
+        {
+            float distance = Vector3.Distance(coin.transform.position, transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                closestCoin = coin;
+            }
+        }
+        return closestCoin.transform;
+    }
+
+    Vector3 FleeFrom(GameObject boo)
+    {
+        Debug.Log("Run!");
+        Vector3 dirToBoo = transform.position - boo.transform.position;
+        Vector3 newPos = transform.position + dirToBoo;
+        return newPos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            score++;
+            Debug.Log("Got a coin!");
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Boo")
+        {
+            Debug.Log("Mamma mia! [score:" + score + "]");
+            Destroy(gameObject);
+        }
+    }
+}
